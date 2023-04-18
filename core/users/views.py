c74e7d6efe1d2495ea8002cbf05aa7e208dfe181
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2023-04-18 10:43:19 +0530

    f

--- core/users/views.py
+++ core/users/views.py
@@ -562,7 +562,9 @@ class PlaceOrderAPI(APIView):
                 "start transaction;"
                 "set @gen_uid = concat(%s, sha1(now()));\
                 set @address_id = (select address_id from customer where user_id = %s); "
-                "INSERT INTO `order` (customer_id, amount, payment_uid, address_id, order_time) VALUES (%s, %s, @gen_uid, @address_id, "+ str(timezone.now())+ " ); ",
+                "INSERT INTO `order` (customer_id, amount, payment_uid, address_id, order_time) VALUES (%s, %s, @gen_uid, @address_id, "+
+                (timezone.now().strftime("%d-%m-%Y %H:%M:%S"))
+                + " ); ",
                 [customer_id, customer_id, customer_id, total],
             )
             cursor.execute("SELECT max(id) FROM `order`;")
